9e1db89426f57880387c6f45a0ae23700ade6809
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-07-09 01:08:59 +0200

    Add styles

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -10,20 +10,41 @@ function User() {
 
   return (
     <Layout
-      title='User'
-      category='Admin'
+      title="User"
+      category="Admin"
       isLoading={isLoading}
       isFetching={isFetching}
       error={error}
     >
-      <div>
-        <h1 className='text-2xl text-center'>Info of {user?.firstName}</h1>
+      <div className="w-4/5 mx-auto h-full flex flex-col gap-5">
+        <h1 className="text-2xl text-center">Info of {user?.firstName}</h1>
 
-        <p>{user?.age}</p>
-        <p>{user?.gender}</p>
-        <p>{user?.address}</p>
-        <p>{user?.phone}</p>
-        <p>{user?.email}</p>
+        <h2 className="text-2xl">
+          Fullname:{' '}
+          <span className="text-white font-semibold">{`${user?.firstName} ${user?.lastName}`}</span>
+        </h2>
+
+        <h2 className="text-2xl">
+          Email: <span className="text-white font-semibold">{user?.email}</span>
+        </h2>
+
+        <h2 className="text-2xl">
+          Age: <span className="text-white font-semibold">{user?.age}</span>
+        </h2>
+
+        <h2 className="text-2xl">
+          Phone: <span className="text-white font-semibold">{user?.phone}</span>
+        </h2>
+
+        <h2 className="text-2xl">
+          Address:{' '}
+          <span className="text-white font-semibold">{user?.address}</span>
+        </h2>
+
+        <h2 className="text-2xl">
+          Gender:{' '}
+          <span className="text-white font-semibold">{user?.gender}</span>
+        </h2>
       </div>
     </Layout>
   )
