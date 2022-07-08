24019c30ba2eb9ec264734baad34e4fb0d5b0fd2
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-07-09 01:08:39 +0200

    updated map functions for universities of user

--- src/pages/UserUnis.tsx
+++ src/pages/UserUnis.tsx
@@ -6,63 +6,51 @@ import useStore from '../store'
 function UserUnis() {
   const { user } = useStore()
 
-  if (user?.universities?.length === 0) return <Layout title={`Universities list of ${user?.firstName}`} category='Users'>
-    <h1>You don't have universities yet</h1>
-  </Layout>
+  if (user?.universities?.length === 0)
+    return (
+      <Layout
+        title={`Universities list of ${user?.firstName}`}
+        category="Users"
+      >
+        <h1>You don't have universities yet</h1>
+      </Layout>
+    )
 
   return (
-    <Layout
-      title={`Universities list of ${user?.firstName}`}
-      category='Users'
-    >
-      <Table hoverable={true} className='w-4/5 mx-auto border-lg dark'>
-      <Table.Head>
-        <Table.HeadCell>
-          Name
-        </Table.HeadCell>
-        <Table.HeadCell>
-          State
-        </Table.HeadCell>
-        <Table.HeadCell>
-          Division
-        </Table.HeadCell>
-        <Table.HeadCell>
-          Academic Rank
-        </Table.HeadCell>
-        <Table.HeadCell>
-          Coachs
-        </Table.HeadCell>
-      </Table.Head>
-      <Table.Body className="divide-y">
-        {user?.universities?.map((uni) => (
-          <Table.Row key={uni.body._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              <Link to={`/unis/${uni.body._id}`}>
-                {uni.body.name}
-              </Link>
-            </Table.Cell>
-            <Table.Cell>
-              {uni.body.state}
-            </Table.Cell>
-            <Table.Cell>
-              {uni.body.division}
-            </Table.Cell>
-            <Table.Cell>
-              {uni.body.academicRank}
-            </Table.Cell>
-            <Table.Cell>
-              {uni.body.coachs.map((item, i) => (
-                <div key={i} className="mb-2">
-                  <div className="text-gray-400">{item.name}</div>
-                  <div className="text-gray-200">{item.email}</div>
-                  <div className="text-gray-200">{item.phone}</div>
-                </div>
-              ))}
+    <Layout title={`Universities list of ${user?.firstName}`} category="Users">
+      <Table hoverable={true} className="w-4/5 mx-auto border-lg dark">
+        <Table.Head>
+          <Table.HeadCell>Name</Table.HeadCell>
+          <Table.HeadCell>State</Table.HeadCell>
+          <Table.HeadCell>Division</Table.HeadCell>
+          <Table.HeadCell>Academic Rank</Table.HeadCell>
+          <Table.HeadCell>Coachs</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {user?.universities?.map((uni) => (
+            <Table.Row
+              key={uni._id}
+              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+            >
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                <Link to={`/unis/${uni._id}`}>{uni.name}</Link>
               </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+              <Table.Cell>{uni.state}</Table.Cell>
+              <Table.Cell>{uni.division}</Table.Cell>
+              <Table.Cell>{uni.academicRank}</Table.Cell>
+              <Table.Cell>
+                {uni.coachs.map((item, i) => (
+                  <div key={i} className="mb-2">
+                    <div className="text-gray-400">{item.name}</div>
+                    <div className="text-gray-200">{item.email}</div>
+                    <div className="text-gray-200">{item.phone}</div>
+                  </div>
+                ))}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </Layout>
   )
 }
