0fb1bd7179eb9e176f8cf3f654a0abda97691f48
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-07-09 01:07:55 +0200

    Added styles and refactorized code

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,23 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Input } from '@nextui-org/react'
-import axios from '../services/axios'
-
-import { User } from '../types'
-
-import { loginSchema, LoginInput } from '../schema/auth.schema'
-
-import useStore from '../store'
 import { AxiosResponse } from 'axios'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from '../components'
+import axios from '../services/axios'
+import { User } from '../types'
+import { loginSchema, LoginInput } from '../schema/auth.schema'
+import useStore from '../store'
+import logo from '../assets/logo.png'
 
 function Login() {
-  const [loginError, setLoginError] = useState<any|null>(null)
+  const [loginError, setLoginError] = useState<any | null>(null)
   const {
     register,
     formState: { errors },
-    handleSubmit
+    handleSubmit,
   } = useForm<LoginInput>({
-    resolver: zodResolver(loginSchema)
+    resolver: zodResolver(loginSchema),
   })
 
   const { setUser } = useStore()
@@ -29,7 +27,10 @@ function Login() {
   async function onSubmit(values: LoginInput) {
     try {
       console.log(values)
-      const { data }: AxiosResponse<User['body']> = await axios.post('/api/login', values)
+      const { data }: AxiosResponse<User['body']> = await axios.post(
+        '/api/login',
+        values
+      )
       console.log(data)
       setUser(data)
       nav('/', { replace: true })
@@ -41,34 +42,66 @@ function Login() {
   console.log({ errors })
 
   return (
-    <div>
-      <p>{loginError}</p>
+    <main className="relative min-h-screen w-full bg-slate-800">
+      <div className="p-6">
+        <header className="flex w-full justify-end">
+          <div>
+            <Link
+              to="/register"
+              className="rounded-2xl border-b-2 border-b-lime-300 bg-lime-400 py-3 px-4 font-bold text-white ring-2 ring-lime-300 hover:bg-lime-600 active:translate-y-[0.125rem] active:border-b-lime-200 transition"
+            >
+              REGISTER
+            </Link>
+          </div>
+        </header>
+        <section className="flex flex-col items-center justify-center mx-auto max-w-sm">
+          <img src={logo} alt="Froz logo" className="my-14" />
+          <div className="space-y-4">
+            <h1 className="text-4xl font-bold mb-6 text-center">Log in</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <Input
-            label='Email'
-            placeholder='janedoe@example.com'
-            type='text'
-            {...register('email')}
-          />
-          <p>{errors.email?.message}</p>
-        </div>
+            <p className="text-center text-red-500 text-md">{loginError}</p>
 
-        <div>
-          <Input.Password
-            label='Password'
-            placeholder='******'
-            type='password'
-            {...register('password')}
-          />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <Input
+                  id="email"
+                  label="Email"
+                  placeholder="janedoe@example.com"
+                  type="text"
+                  {...register('email')}
+                />
+                <p>{errors.email?.message}</p>
+              </div>
 
-          <p>{errors.password?.message}</p>
-        </div>
+              <div>
+                <Input
+                  id="password"
+                  label="Password"
+                  placeholder="******"
+                  type="password"
+                  {...register('password')}
+                />
 
-        <button type="submit" className='px-5 py-2 bg-slate-200 text-blue-900 rounded-full'>SUBMIT</button>
-      </form>
-    </div>
+                <p>{errors.password?.message}</p>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full rounded-2xl border-b-4 border-b-lime-600 bg-lime-500 py-3 font-bold text-white hover:bg-lime-400 active:translate-y-[0.125rem] active:border-b-lime-700 my-4"
+              >
+                LOG IN
+              </button>
+            </form>
+            <p className="text-white text-sm">
+              Go to{' '}
+              <Link to="/register" className="text-lime-500 hover:underline">
+                Register Page
+              </Link>
+            </p>
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import axios from '../services/axios'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Input } from '@nextui-org/react'
-
+import { Input } from '../components'
 import { registerSchema, RegisterUserInput } from '../schema/auth.schema'
+import axios from '../services/axios'
+import logo from '../assets/logo.png'
 
 function Register() {
-  const [registerError, setRegisterError] = useState<any|null>(null)
+  const [registerError, setRegisterError] = useState<any | null>(null)
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<RegisterUserInput>({
-    resolver: zodResolver(registerSchema)
+    resolver: zodResolver(registerSchema),
   })
 
   const nav = useNavigate()
@@ -23,113 +23,153 @@ function Register() {
     try {
       console.log(values)
       console.log('Enviando..xd')
-      const res = await axios.post('/api/users', values);
+      const res = await axios.post('/api/users', values)
       console.log(res)
       nav('/', { replace: true })
     } catch (e: any) {
-      setRegisterError(e.message);
+      setRegisterError(e.message)
     }
   }
 
-  console.log({ errors });
+  console.log({ errors })
 
   return (
-    <div>
-      <p>{registerError}</p>
+    <main className="relative min-h-screen w-full bg-slate-800">
+      <div className="p-6">
+        <header className="flex w-full justify-end">
+          <div>
+            <Link
+              to="/login"
+              className="rounded-2xl border-b-2 border-b-lime-300 bg-lime-400 py-3 px-4 font-bold text-white ring-2 ring-lime-300 hover:bg-lime-600 active:translate-y-[0.125rem] active:border-b-lime-200 transition"
+            >
+              LOGIN
+            </Link>
+          </div>
+        </header>
+        <section className="flex flex-col items-center justify-center mx-auto max-w-sm">
+          <img src={logo} alt="Froz logo" className="my-14" />
+          <div className="space-y-4">
+            <h1 className="mb-6 text-4xl font-bold">Create your profile</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-element">
-          <Input
-            label='email'
-            type="email"
-            placeholder="jane.doe@example.com"
-            {...register("email")}
-          />
-          <p>{errors.email?.message}</p>
-        </div>
+            <p>{registerError}</p>
 
-        <div className="form-element">
-          <Input
-            label='First Name'
-            type="text"
-            placeholder="Jane Doe"
-            {...register("firstName")}
-          />
-          <p>{errors.firstName?.message}</p>
-        </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <Input
+                  id="email"
+                  label="Email"
+                  type="email"
+                  placeholder="jane.doe@example.com"
+                  {...register('email')}
+                />
+                <p>{errors.email?.message}</p>
+              </div>
 
-        <div className="form-element">
-          <Input
-            label='Last Name'
-            type="text"
-            placeholder="Jane Doe"
-            {...register("lastName")}
-          />
-          <p>{errors.lastName?.message}</p>
-        </div>
+              <div>
+                <Input
+                  id="email"
+                  label="First Name"
+                  type="text"
+                  placeholder="Jane Doe"
+                  {...register('firstName')}
+                />
+                <p>{errors.firstName?.message}</p>
+              </div>
 
-        <div className="form-element">
-          <Input
-            label='Age'
-            type="text"
-            placeholder="19"
-            {...register("age")}
-          />
-          <p>{errors.age?.message}</p>
-        </div>
+              <div>
+                <Input
+                  id="lastName"
+                  label="Last Name"
+                  type="text"
+                  placeholder="Jane Doe"
+                  {...register('lastName')}
+                />
+                <p>{errors.lastName?.message}</p>
+              </div>
 
-        <div className="form-element">
-          <Input
-            label='Gender'
-            type="text"
-            placeholder="Female"
-            {...register("gender")}
-          />
-          <p>{errors.gender?.message}</p>
-        </div>
+              <div>
+                <Input
+                  id="password"
+                  label="Password"
+                  type="password"
+                  placeholder="*********"
+                  {...register('password')}
+                />
+                <p>{errors.password?.message}</p>
+              </div>
 
-        <div className="form-element">
-          <Input
-            label='Address'
-            type="text"
-            placeholder="Street +12 Venecia -a"
-            {...register("address")}
-          />
-          <p>{errors.address?.message}</p>
-        </div>
+              <div>
+                <Input
+                  id="confirmpassword"
+                  label="Confirm Password"
+                  type="password"
+                  placeholder="*********"
+                  {...register('passwordConfirmation')}
+                />
+                <p>{errors.passwordConfirmation?.message}</p>
+              </div>
 
-        <div className="form-element">
-          <Input
-            label='Phone'
-            type="text"
-            placeholder="+1 123 132 567"
-            {...register("phone")}
-          />
-          <p>{errors.phone?.message}</p>
-        </div>
+              <div>
+                <Input
+                  id="age"
+                  label="Age"
+                  type="text"
+                  placeholder="19"
+                  {...register('age')}
+                />
+                <p>{errors.age?.message}</p>
+              </div>
 
-        <div className="form-element">
-          <Input.Password
-            label='Password'
-            // type="password"
-            placeholder="*********"
-            {...register("password")}
-          />
-          <p>{errors.password?.message}</p>
-        </div>
+              <div>
+                <Input
+                  id="gender"
+                  label="Gender"
+                  type="text"
+                  placeholder="Female"
+                  {...register('gender')}
+                />
+                <p>{errors.gender?.message}</p>
+              </div>
 
-        <div className="form-element">
-          <Input.Password
-            label='Confirm Password'
-            // type="password"
-            placeholder="*********"
-            {...register("passwordConfirmation")}
-          />
-          <p>{errors.passwordConfirmation?.message}</p>
-        </div>
-        <button type="submit" className='px-5 py-2 bg-slate-200 text-blue-900 rounded-full'>SUBMIT</button>
-      </form>
-    </div>
+              <div>
+                <Input
+                  id="address"
+                  label="Address"
+                  type="text"
+                  placeholder="Street +12 Venecia -a"
+                  {...register('address')}
+                />
+                <p>{errors.address?.message}</p>
+              </div>
+
+              <div>
+                <Input
+                  id="phone"
+                  label="Phone"
+                  type="text"
+                  placeholder="+1 123 132 567"
+                  {...register('phone')}
+                />
+                <p>{errors.phone?.message}</p>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full rounded-2xl border-b-4 border-b-lime-600 bg-lime-500 py-3 font-bold text-white hover:bg-lime-400 active:translate-y-[0.125rem] active:border-b-lime-700 my-4"
+              >
+                REGISTER
+              </button>
+            </form>
+            <p className="text-white text-sm">
+              Go to{' '}
+              <Link to="/login" className="text-lime-500 hover:underline">
+                Login Page
+              </Link>
+            </p>
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
 
