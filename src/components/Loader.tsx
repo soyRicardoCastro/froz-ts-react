32a8dcbd863593bc967dc7f8edad532c4380b2ee
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-07-13 03:04:21 +0200

    Put margin left 50px to Loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -9,9 +9,24 @@ interface Props {
 }
 
 function Loader({ isFetching, isLoading, error }: Props['body']) {
-  if (isLoading) return <Loading color="secondary">Loading data...</Loading>
-  if (isFetching) return <Loading color="secondary">Refreshing data...</Loading>
-  if (error) return <h1 className="text-2xl text-red-600 font-bold">Internal server error :/</h1>
+  if (isLoading)
+    return (
+      <Loading className="ml-14" color="success">
+        Loading data...
+      </Loading>
+    )
+  if (isFetching)
+    return (
+      <Loading className="ml-14" color="success">
+        Refreshing data...
+      </Loading>
+    )
+  if (error)
+    return (
+      <h1 className="text-2xl text-red-600 font-bold">
+        Internal server error :/
+      </h1>
+    )
 
   return <div></div>
 }
